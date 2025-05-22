be59b8e48a72729afdf87b41e0739283df0bcc00
Thur-hub15 <arthurclaro721@gmail.com>
2025-05-22 17:43:44 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,70 @@
+#comentario
+#dados primitivos
+
+#strings  =textos
+
+print('olá mundo!')
+print('Sejam bem vindos', 'pessoas')
+print('meu nome é', 'Arthur')
+print('-------------')
+
+
+#dado do tipo inteiro(int)
+
+print(0)
+print(-200)
+print(300)
+
+#dado do tipo real (float)
+print(1.5)
+print(10.0)
+print(10.8)
+print(10.0)
+
+
+
+#dado dotipo boleano(bool)
+
+print(True)
+print(False)
+
+
+# input () = guardar um dado
+
+print(input('Digite seu nome '))
+
+
+# variaveis:
+
+nome  = 'Arthur'
+idade  = 15
+endereco = 'Rua bras, adamastor'
+curso = 'programacao em python'
+
+print('Ola meu nome é', nome)
+print('minha idade é', idade)
+print('minha casa está no endereço', endereco)
+print('faço o curso', curso)
+
+
+
+
+
+
+# calculadora de soma
+
+numero_1 = int(input('digite um número'))
+numero_2 = int(input('outro número'))
+
+print(numero_1 + numero_2)
+# soma de  2 fluxosgrama
+# 4 funções básicas
+n1 = float(input('Numero 1 :'))
+n2 = float(input('Numero 2 :'))
+
+print('resultado soma', n1 + n2)
+print('resultado subtração ', n1 -  n2)
+print('resultado multiplicação', n1 *  n2)
+print('resultado divisão ', n1 /  n2)
+
+
