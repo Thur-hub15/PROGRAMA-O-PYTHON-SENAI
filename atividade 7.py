2d3dd09669a6b72696bf6a0faaa281284a7adeff
Thur-hub15 <arthurclaro721@gmail.com>
2025-05-29 16:27:07 -0300

    Add files via upload

---
+++ atividade 7.py
@@ -0,0 +1,25 @@
+# 0
+numeros = list(range(0,21,2))
+print(numeros)
+
+# 1
+numeros = list[0,1,2,3,4,5,6,7,8,9,10]
+print(numeros)
+
+
+#2
+numeros = [1,2,3,4,5,6]
+print(numeros[3])
+
+# 3
+numeros.append(9)
+print(numeros)
+
+# 4
+del numeros[5]
+print(numeros)
+ 
+ #5
+carros = ['Uno, Supra, Geta']
+carros +=(numeros)
+print(carros)
