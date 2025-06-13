82f4c60123a7c81024825bd3f34080ea7e8c8c14
Thur-hub15 <arthurclaro721@gmail.com>
2025-06-13 17:46:10 -0300

    Add files via upload

---
+++ Aula18.py
@@ -0,0 +1,21 @@
+ # aula 18
+
+Scrum Master: Facilita o processo ágil, remove impedimentos e garante que a equipe siga os princípios do Scrum. Sua função é essencial para criar um ambiente de trabalho eficiente e colaborativo, promovendo a melhoria contínua da equipe.
+
+Product Owner (PO): Responsável por definir a visão do produto e priorizar o backlog. Sua importância está em garantir que o time de desenvolvimento trabalhe nas funcionalidades que agregam mais valor ao negócio e atendem às necessidades dos stakeholders.
+
+Líder de Equipe de TI: Coordena e orienta a equipe técnica, assegurando que os prazos e a qualidade sejam cumpridos. Sua função é essencial para garantir uma execução técnica eficaz e manter a comunicação entre a equipe e outras áreas.
+
+Equipe de Tecnologia: Composta por desenvolvedores, testadores e outros profissionais, executa as tarefas do projeto. Sua importância reside na entrega das soluções técnicas que atendem aos requisitos do produto, garantindo a qualidade e funcionalidade.
+
+Scrum Master e PO em Conjunto: Trabalham juntos para garantir alinhamento entre as necessidades do negócio e a execução técnica, promovendo uma comunicação eficiente e a entrega de valor contínuo.
+
+Função do Scrum Master: Atua como um líder servidor, ajudando a remover bloqueios e a melhorar o desempenho da equipe, garantindo que o processo ágil seja seguido corretamente.
+
+Função do PO: Prioriza as funcionalidades e mantém o foco no valor do produto, garantindo que o time de TI tenha uma direção clara e objetiva no desenvolvimento do projeto.
+
+Líder de TI: Gerencia os aspectos técnicos do projeto, resolvendo problemas e orientando a equipe para que as entregas sejam feitas com qualidade e eficiência.
+
+Equipe de Tecnologia e Colaboração: A equipe técnica é crucial para transformar as ideias do PO em soluções concretas, com a colaboração sendo a chave para resolver desafios e entregar resultados.
+
+Importância do Alinhamento: A interação entre Scrum Master, PO, Líder de TI e a equipe de tecnologia é fundamental para o sucesso do projeto, garantindo que todos trabalhem alinhados em direção aos objetivos comun
